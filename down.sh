7055a9ea08b6770d08145522a36144fcf62203a8
sam@23.199 <you@example.com>
2022-07-15 17:01:45 +0800

    data/xx.tar.gz

--- down.sh
+++ down.sh
@@ -8,11 +8,11 @@ ZLIB_VERSION="1.2.12"
 ZLIB_CHECKSUM="91844808532e5ce316b3c010929493c0244f3d37593afd6de04f71821d5136d9"
 
 ADD="curl -fSL -k"
-$ADD https://nginx.org/download/nginx-$VERSION.tar.gz > nginx.tar.gz
-$ADD https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz > openssl.tar.gz
-$ADD https://zlib.net/zlib-$ZLIB_VERSION.tar.gz > zlib.tar.gz
+# $ADD https://nginx.org/download/nginx-$VERSION.tar.gz > data/nginx.tar.gz
+# $ADD https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz > data/openssl.tar.gz
+$ADD https://zlib.net/zlib-$ZLIB_VERSION.tar.gz > data/zlib.tar.gz
 
 # validate
-test "$(sha256sum nginx.tar.gz | awk '{print $1}')" == "$CHECKSUM" ] || echo "err: nginx.tar.gz"
-test "$(sha256sum openssl.tar.gz | awk '{print $1}')" == "$OPENSSL_CHECKSUM" || echo "err: openssl.tar.gz"
-test "$(sha256sum zlib.tar.gz | awk '{print $1}')" == "$ZLIB_CHECKSUM"  || echo "err: zlib.tar.gz"
+test "$(sha256sum data/nginx.tar.gz | awk '{print $1}')" == "$CHECKSUM" || echo "err: nginx.tar.gz"
+test "$(sha256sum data/openssl.tar.gz | awk '{print $1}')" == "$OPENSSL_CHECKSUM" || echo "err: openssl.tar.gz"
+test "$(sha256sum data/zlib.tar.gz | awk '{print $1}')" == "$ZLIB_CHECKSUM"  || echo "err: zlib.tar.gz"
