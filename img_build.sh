480a1128e836977e5e99d4b73c20aaddc313a21c
sam@23.199 <you@example.com>
2022-07-15 18:03:00 +0800

    +img_build.sh

---
+++ img_build.sh
@@ -0,0 +1,20 @@
+
+cur=$(cd "$(dirname "$0")"; pwd)
+cd $cur
+
+#echo "export DOCKER_REGISTRY_USER_sdsir=xxx" >> /etc/profile
+#echo "export DOCKER_REGISTRY_PW_sdsir=xxx" >> /etc/profile
+
+source /etc/profile
+export |grep DOCKER_REG
+repo=registry.cn-shenzhen.aliyuncs.com
+echo "${DOCKER_REGISTRY_PW_infrastSubUser2}" |docker login --username=${DOCKER_REGISTRY_USER_infrastSubUser2} --password-stdin $repo
+
+ns=infrastlabs
+# cache="--no-cache"
+# pull="--pull"
+ver=latest #02: +full; 04: bins;
+img="build-nginx:$ver"
+
+docker build $cache $pull -t $repo/$ns/$img -f Dockerfile . 
+docker push $repo/$ns/$img
