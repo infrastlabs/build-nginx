2d3c1c3f376c8efda69663ba394b57ee67d0a5fc
sam#gemmi-win10 <sam@china.gz>
2023-02-15 20:53:05 +0800

    fix logic

--- img_build.sh
+++ img_build.sh
@@ -9,7 +9,7 @@ source /etc/profile
 export |grep DOCKER_REG
 repo=registry.cn-shenzhen.aliyuncs.com
 echo "${DOCKER_REGISTRY_PW_infrastSubUser2}" |docker login --username=${DOCKER_REGISTRY_USER_infrastSubUser2} --password-stdin $repo
-test -z "uname -a |grep aarch64" && arch=arm64 || arch=x64
+test -z "uname -a |grep aarch64" && arch=x64 || arch=arm64
 
 
 ns=infrastlabs
