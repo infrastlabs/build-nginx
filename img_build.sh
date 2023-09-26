13a2390695d3499c4d3e5d1958a8337a87539df0
sam@host-barge-headless <you@example.com>
2023-09-26 17:02:34 +0800

    img_build: ver=latest-echo

--- img_build.sh
+++ img_build.sh
@@ -15,7 +15,7 @@ test -z "$(uname -a |grep aarch64)" && arch=x64 || arch=arm64
 ns=infrastlabs
 # cache="--no-cache"
 # pull="--pull"
-ver=latest #02: +full; 04: bins;
+ver=latest-echo #02: +full; 04: bins;
 img="build-nginx:$arch-$ver"
 
 docker build $cache $pull -t $repo/$ns/$img -f Dockerfile . 
